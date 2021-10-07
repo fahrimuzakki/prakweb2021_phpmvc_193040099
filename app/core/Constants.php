afed40b3eab9175b2773d56fe310fb9f82be2bd2
fahrimuzakki_ <58680886+fahrimuzakki@users.noreply.github.com>
2021-10-07 22:47:21 +0700

    Video 7 - Model

---
+++ app/core/Constants.php
@@ -0,0 +1,3 @@
+<?php
+
+define('BASEURL', 'http://localhost/phpmvc_193040099/public');
---
+++ app/models/User_model.php
@@ -0,0 +1,8 @@
+<?php 
+class User_model {
+    public $nama = 'Mohamad Fahri Muzakki';
+    public function getUser()
+    {
+        return $this->nama;
+    }
+}
