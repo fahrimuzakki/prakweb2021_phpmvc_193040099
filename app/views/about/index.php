9ab1eeca6e95da91a6792272e015c3d4b451b218
fahrimuzakki_ <58680886+fahrimuzakki@users.noreply.github.com>
2021-10-07 23:11:54 +0700

    Video 7 - Model

---
+++ app/views/about/index.php
@@ -0,0 +1,6 @@
+<div class="container">
+        <h1 class="mt-4">About Me</h1>
+        <img src="<?= BASEURL; ?>/img/profile.jpeg" alt="Mohamad Fahri Muzakki
+        "width="200" class="rounded-circle shadow">
+        <p>Hallo, Nama Saya <?= $data['nama']; ?>, Umur Saya <?= $data['umur']; ?> Tahun, Saya Adalah Seorang <?= $data['pekerjaan']; ?>.</p>
+        </div>
---
+++ app/views/home/index.php
@@ -0,0 +1,12 @@
+<div class="container">
+
+
+<div class="jumbotron mt-4">
+    <h1 class="display-4">Selamat Datang di Website Saya!</h1>
+    <P class="lead">Halo, Nama Saya <?= $data['nama']; ?>
+</p>
+    <hr class="my-4">
+    <p>Hello Riiiiiiiiiiiiiii</p>
+    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+</div>
+</div>
